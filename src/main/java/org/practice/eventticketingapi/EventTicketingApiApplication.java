a6a65b29df00a9918bb72ef8156871933062b785
Mouad Lasri <mouadkor20@gmail.com>
2026-06-06 16:18:54 +0900

    first commit

---
+++ src/main/java/org/practice/eventticketingapi/EventTicketingApiApplication.java
@@ -0,0 +1,13 @@
+package org.practice.eventticketingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTicketingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/practice/eventticketingapi/EventTicketingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.eventticketingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
